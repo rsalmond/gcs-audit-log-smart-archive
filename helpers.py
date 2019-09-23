d70947e6739886b7c0c8db7a9054abbd62377d23
Dom Zippilli <domz@google.com>
2019-09-23 21:06:58 +0000

    adds QoL features, refactor to shorten file and reduce repetition,

---
+++ helpers.py
@@ -0,0 +1,184 @@
+from datetime import datetime, timezone
+from queue import Queue
+
+from dateutil import parser as dateparser
+from google.api_core.exceptions import BadRequest
+from google.cloud import bigquery, storage
+
+
+def event_is_fresh(data, context):
+    """Ensure a background Cloud Function only executes within a certain
+    time period after the triggering event.
+
+    Args:
+        data (dict): The event payload.
+        context (google.cloud.functions.Context): The event metadata.
+    Returns:
+        None; output is written to Stackdriver Logging
+    """
+    if data is None:
+        # desktop run
+        return True
+
+    timestamp = context.timestamp
+
+    event_time = dateparser.parse(timestamp)
+    event_age = (datetime.now(timezone.utc) - event_time).total_seconds()
+    event_age_ms = event_age * 1000
+
+    # Ignore events that are too old
+    # TODO: Should this be configurable?
+    max_age_ms = 10000
+    if event_age_ms > max_age_ms:
+        print('Event timeout. Dropping {} (age {}ms)'.format(
+            context.event_id, event_age_ms))
+        return False
+    return True
+
+
+def load_config_file(filepath, required=[]):
+    """
+    Loads configuration file into module variables.
+    """
+    config = dict()
+    config_file = open(filepath, "r")
+    for line in config_file:
+        # ignore comments
+        if line.startswith('#'):
+            continue
+        # parse the line
+        tokens = line.split('=')
+        if len(tokens) != 2:
+            print("Error parsing config tokens: %s" % tokens)
+            continue
+        k, v = tokens
+        config[k.strip()] = v.strip()
+    # quick validation
+    for r in required:
+        if r not in config.keys() or config[r] is "CONFIGURE_ME":
+            print('Missing required config item: {}'.format(r))
+            exit(1)
+    return config
+
+
+clients = {}
+
+
+def get_bq_client():
+    """Get a BigQuery client. Uses a simple create-if-not-found mechanism to avoid repeatedly creating new clients.
+
+    Returns:
+        google.cloud.bigquery.Client -- A BigQuery client.
+    """
+    if 'bq' not in clients:
+        bq = bigquery.Client()
+        clients['bq'] = bq
+    return clients['bq']
+
+
+def get_gcs_client():
+    """Get a GCS client. Uses a simple create-if-not-found mechanism to avoid repeatedly creating new clients.
+
+    Returns:
+        google.cloud.storage.Client -- A GCS client.
+    """
+    if 'gcs' not in clients:
+        gcs = storage.Client()
+        clients['gcs'] = gcs
+    return clients['gcs']
+
+
+def initialize_table(name, schema):
+    """Creates, if not found, a table.
+
+    Arguments:
+        name {string} -- The fully qualified table name.
+        schema {string} -- The schema portion of a BigQuery CREATE TABLE DDL query. For example: "resourceName STRING"
+
+    Returns:
+        google.cloud.bigquery.table.RowIterator -- Result of the query. Since this is a DDL query, this will always be empty if it succeeded.
+
+    Raises:
+        google.cloud.exceptions.GoogleCloudError –- If the job failed.
+        concurrent.futures.TimeoutError –- If the job did not complete in the given timeout.
+    """
+    bq = get_bq_client()
+
+    querytext = """
+        CREATE TABLE IF NOT EXISTS {} (
+        {}
+        )""".format(name, schema)
+
+    query_job = bq.query(querytext)
+    return query_job.result()
+
+
+def get_bucket_and_object(resource_name):
+    """Given an audit log resourceName, parse out the bucket name and object path within the bucket.
+
+    Returns:
+        (str, str) -- ([bucket name], [object name])
+    """
+    pathparts = resource_name.split("buckets/", 1)[1].split("/", 1)
+
+    bucket_name = pathparts[0]
+    object_name = pathparts[1].split("objects/", 1)[1]
+
+    return (bucket_name, object_name)
+
+
+def bq_insert_stream(tablename, iter_q, batch_size):
+    """Insert records from an IterableQueue into BigQuery.
+
+    Arguments:
+        table_name {str} -- The name of the table into which to stream rows.
+        iter_q {IterableQueue} -- The IterableQueue to read from.
+
+    Returns:
+        google.cloud.bigquery.table.RowIterator -- Result of the query. Since this is an INSERT query, this will always be empty if it succeeded.
+
+    Raises:
+        google.cloud.exceptions.GoogleCloudError –- If the job failed.
+        concurrent.futures.TimeoutError –- If the job did not complete in the given timeout.
+    """
+    bq = get_bq_client()
+
+    insert_errors = []
+    batch = []
+    print("Starting BQ insert stream to {}...".format(tablename))
+    for row in iter_q:
+        batch.append(row)
+        if len(batch) > batch_size:
+            try:
+                insert_errors.append(
+                    bq.insert_rows_json(tablename, batch))
+            except BadRequest as e:
+                if not e.message.endswith("No rows present in the request."):
+                    raise e
+            finally:
+                batch.clear()
+    return("Finished BQ insert stream to {}.\nErrors: {}".format(
+        tablename, [x for x in flatten(insert_errors)]
+    ))
+
+
+def flatten(iterable, iter_types=(list, tuple)):
+    """Flattens nested iterables into a flat iterable.
+    """
+    for i in iterable:
+        if isinstance(i, iter_types):
+            for j in flatten(i, iter_types):
+                yield j
+        else:
+            yield i
+
+
+class IterableQueue(Queue):
+
+    _sentinel = object()
+
+    def __iter__(self):
+        return iter(self.get, self._sentinel)
+
+    def close(self):
+        self.put(self._sentinel)
--- main.py
+++ main.py
@@ -1,54 +1,30 @@
 #!/usr/bin/env python3
 import warnings
-from atexit import register
-from atexit import unregister
-from concurrent.futures import as_completed
-from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
-from datetime import timezone
-from queue import Queue
+from atexit import register, unregister
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime, timezone
+from os import getenv
 from sys import exit
 
-from dateutil import parser
-from google.api_core.exceptions import BadRequest
 from google.api_core.exceptions import NotFound
-from google.cloud import bigquery
-from google.cloud import storage
+from google.cloud import bigquery, storage
+
+from helpers import (IterableQueue, bq_insert_stream, event_is_fresh,
+                     get_bq_client, get_bucket_and_object, get_gcs_client,
+                     initialize_table, load_config_file)
 
 warnings.filterwarnings(
     "ignore", "Your application has authenticated using end user credentials")
 
 
-config = {
-    "CONFIG_FILE_PATH": "./config.cfg"
-}
-
-
-def load_config():
-    """
-    Loads configuration file into module variables.
-    """
-    config_file = open(config["CONFIG_FILE_PATH"], "r")
-    for line in config_file:
-        # ignore comments
-        if line.startswith('#'):
-            continue
-        # parse the line
-        tokens = line.split('=')
-        if len(tokens) != 2:
-            print("Error parsing config tokens: %s" % tokens)
-            continue
-        k, v = tokens
-        config[k.strip()] = v.strip()
-    # quick validation
-    for required in [
-        'PROJECT',
-        'DATASET_NAME',
-        'DAYS_THRESHOLD',
-            'NEW_STORAGE_CLASS']:
-        if required not in config.keys() or config[required] is "CONFIGURE_ME":
-            print('Missing required config item: {}'.format(required))
-            exit(1)
+config_file = getenv("SMART_ARCHIVE_CONFIG") if getenv("SMART_ARCHIVE_CONFIG") else "./config.cfg"
+print("Loading config: {}".format(config_file))
+config = load_config_file(config_file, required=[
+    'PROJECT',
+    'DATASET_NAME',
+    'DAYS_THRESHOLD',
+    'NEW_STORAGE_CLASS',
+    'BQ_BATCH_WRITE_SIZE'])
 
 
 def initialize_moved_objects_table():
@@ -61,21 +37,76 @@ def initialize_moved_objects_table():
         google.cloud.exceptions.GoogleCloudError –- If the job failed.
         concurrent.futures.TimeoutError –- If the job did not complete in the given timeout.
     """
-    bq = get_bq_client()
-
     moved_objects_table = "`{}.{}.objects_moved_to_{}`".format(
         config['PROJECT'], config['DATASET_NAME'], config['NEW_STORAGE_CLASS'])
-
-    querytext = """
-        CREATE TABLE IF NOT EXISTS {} (
+    schema = """
             resourceName STRING, 
             size INT64,
             archiveTimestamp TIMESTAMP
-        )""".format(
-        moved_objects_table)
+        """
+    return initialize_table(moved_objects_table, schema)
 
-    query_job = bq.query(querytext)
-    return query_job.result()
+
+def initialize_excluded_objects_table():
+    """Creates, if not found, a table in which objects which should be ignored by this script are stored.
+
+    Returns:
+        google.cloud.bigquery.table.RowIterator -- Result of the query. Since this is a DDL query, this will always be empty if it succeeded.
+
+    Raises:
+        google.cloud.exceptions.GoogleCloudError –- If the job failed.
+        concurrent.futures.TimeoutError –- If the job did not complete in the given timeout.
+    """
+    excluded_objects_table = "`{}.{}.objects_excluded_from_archive`".format(
+        config['PROJECT'], config['DATASET_NAME'])
+    schema = "resourceName STRING"
+    return initialize_table(excluded_objects_table, schema)
+
+
+moved_objects = IterableQueue()
+
+
+def moved_objects_insert_stream():
+    """Insert the resource name of an object into the table of moved objects for exclusion later.
+
+    Arguments:
+        resource_name {str} -- The resource name of the object, as given in the audit log.
+
+    Returns:
+        google.cloud.bigquery.table.RowIterator -- Result of the query. Since this is an INSERT query, this will always be empty if it succeeded.
+
+    Raises:
+        google.cloud.exceptions.GoogleCloudError –- If the job failed.
+        concurrent.futures.TimeoutError –- If the job did not complete in the given timeout.
+    """
+    # TODO: configurable?
+    moved_objects_table = "{}.{}.objects_moved_to_{}".format(
+        config['PROJECT'], config['DATASET_NAME'], config['NEW_STORAGE_CLASS'])
+    return bq_insert_stream(moved_objects_table, moved_objects, config["BQ_BATCH_WRITE_SIZE"])
+
+
+excluded_objects = IterableQueue()
+
+
+def excluded_objects_insert_stream():
+    """Insert the resource name of an object into the table of excluded objects.
+
+    Arguments:
+        resource_name {str} -- The resource name of the object, as given in the audit log.
+
+    Returns:
+        google.cloud.bigquery.table.RowIterator -- Result of the query. Since this is an INSERT query, this will always be empty if it succeeded.
+
+    Raises:
+        google.cloud.exceptions.GoogleCloudError –- If the job failed.
+        concurrent.futures.TimeoutError –- If the job did not complete in the given timeout.
+    """
+
+    # TODO: configurable?
+    excluded_objects_table = "{}.{}.objects_excluded_from_archive".format(
+        config['PROJECT'], config['DATASET_NAME'])
+
+    return bq_insert_stream(excluded_objects_table, excluded_objects, config["BQ_BATCH_WRITE_SIZE"])
 
 
 def query_access_table():
@@ -98,6 +129,9 @@ def query_access_table():
     moved_objects_table = "`{}.{}.objects_moved_to_{}`".format(
         config['PROJECT'], config['DATASET_NAME'], config['NEW_STORAGE_CLASS'])
 
+    excluded_objects_table = "`{}.{}.objects_excluded_from_archive`".format(
+        config['PROJECT'], config['DATASET_NAME'])
+
     querytext = """
     SELECT a.resourceName, lastAccess FROM (
         SELECT REGEXP_REPLACE(protopayload_auditlog.resourceName, "gs://.*/", "") AS resourceName,
@@ -105,72 +139,50 @@ def query_access_table():
         GROUP BY resourceName) 
     AS a 
     LEFT JOIN {1} as b ON a.resourceName = b.resourceName
-    WHERE b.resourceName IS NULL
-""".format(access_log_tables, moved_objects_table)
+    LEFT JOIN {2} as c ON a.resourceName = c.resourceName
+    WHERE b.resourceName IS NULL AND c.resourceName IS NULL
+    """.format(access_log_tables, moved_objects_table, excluded_objects_table)
     query_job = bq.query(querytext)
     return query_job.result()
 
 
-class IterableQueue(Queue):
-
-    _sentinel = object()
-
-    def __iter__(self):
-        return iter(self.get, self._sentinel)
-
-    def close(self):
-        self.put(self._sentinel)
-
-
-moved_objects = IterableQueue()
-
-def flatten(iterable, iter_types=(list, tuple)):
-    """Flattens nested iterables into a flat iterable.
-    """
-    for i in iterable:
-        if isinstance(i, iter_types):
-            for j in flatten(i, iter_types):
-                yield j
-        else:
-            yield i
-
-
-def moved_objects_insert_stream():
-    """Insert the resource name of an object into the table of moved objects for exclusion later.
+def archive_object(resourceName, bucket_name, object_name, object_path):
+    """Rewrites an object to the archive storage class and stores record of it.
 
     Arguments:
-        resource_name {str} -- The resource name of the object, as given in the audit log.
+        resourceName {string} -- The resource name for the object as shown in the audit log (protopayload_auditlog.resourceName).
+        bucket_name {string} -- The name of the bucket where the object resides.
+        object_name {string} -- The name of the object within the bucket.
+        object_path {string} -- The full gs:// path to the object.
 
     Returns:
-        google.cloud.bigquery.table.RowIterator -- Result of the query. Since this is an INSERT query, this will always be empty if it succeeded.
-
-    Raises:
-        google.cloud.exceptions.GoogleCloudError –- If the job failed.
-        concurrent.futures.TimeoutError –- If the job did not complete in the given timeout.
+        string -- Human-readable output describing the operations undertaken.
     """
-    bq = get_bq_client()
+    gcs = get_gcs_client()
+    bucket = storage.bucket.Bucket(gcs, name=bucket_name)
+    output = []
+    try:
+        blob = storage.blob.Blob(object_name, bucket)
+        blob.update_storage_class(config['NEW_STORAGE_CLASS'])
+        output.append("\tRewrote {} to: {}".format(
+            object_path, config['NEW_STORAGE_CLASS']))
+        moved_objects.put(
+            {
+                "resourceName": resourceName,
+                "size": blob.size,
+                "archiveTimestamp": str(datetime.now(timezone.utc))
+            }, True)
+        output.append(
+            "\tStreaming {} object archive status to BQ.".format(object_path))
+    except NotFound:
+        output.append("Skipping {} :: This object wasn't found. Adding to excluded objects list so it will no longer be considered.".format(
+            object_path))
+        excluded_objects.put(
+            {
+                "resourceName": resourceName
+            }, True)
+    return '\n'.join(output)
 
-    # TODO: configurable?
-    moved_objects_table = "{}.{}.objects_moved_to_{}".format(
-        config['PROJECT'], config['DATASET_NAME'], config['NEW_STORAGE_CLASS'])
-
-    insert_errors = []
-    batch = []
-    print("Starting BQ insert stream...")
-    for row in moved_objects:
-        batch.append(row)
-        if len(batch) > config["BQ_BATCH_WRITE_SIZE"]:
-            try:
-                #XXX: this could OOM if BQ is having a bad day.
-                insert_errors.append(bq.insert_rows_json(moved_objects_table, batch))
-            except BadRequest as e:
-                if not e.message.endswith("No rows present in the request."):
-                    raise e
-            finally:
-                batch.clear()
-
-    print("Finished BQ insert stream...")
-    return "BQ Errors:\t{}".format([x for x in flatten(insert_errors)])
 
 def evaluate_objects(audit_log):
     """Evaluates objects in the audit log to see if they should be moved to a new storage class.
@@ -179,39 +191,20 @@ def evaluate_objects(audit_log):
         audit_log {google.cloud.bigquery.table.RowIterator} -- The result set of a query of the audit log table, with the columns `resourceName` and `lastAccess`.
     """
 
-    # This function will be run o(objects) times in the executor pool
-    def _archive_object(row, bucket_name, object_name, object_path):
-        gcs = get_gcs_client()
-        bucket = storage.bucket.Bucket(gcs, name=bucket_name)
-        output = []
-        try:
-            blob = storage.blob.Blob(object_name, bucket)
-            blob.update_storage_class(config['NEW_STORAGE_CLASS'])
-            output.append("\tRewrote {} to: {}".format(
-                object_path, config['NEW_STORAGE_CLASS']))
-            moved_objects.put(
-                {
-                    "resourceName": row.resourceName,
-                    "size": blob.size,
-                    "archiveTimestamp": str(datetime.now(timezone.utc))
-                }, True)
-            output.append(
-                "\tStreaming {} object archive status to BQ.".format(object_path))
-        except NotFound:
-            output.append("Skipping {} :: this object seems to have been deleted.".format(
-                object_path))
-        return '\n'.join(output)
-
     with ThreadPoolExecutor(max_workers=8) as executor:
         # start BQ stream
-        stream_future = executor.submit(moved_objects_insert_stream)
+        move_stream_future = executor.submit(moved_objects_insert_stream)
+        exclude_stream_future = executor.submit(excluded_objects_insert_stream)
         archive_futures = []
 
         def cleanup():
             # terminate the BQ stream
             moved_objects.close()
+            excluded_objects.close()
             executor.shutdown()
-            print(stream_future.result())
+            print(move_stream_future.result())
+            print(exclude_stream_future.result())
+
         # shutdown hook for cleanup in case we get a sigterm
         register(cleanup)
 
@@ -225,11 +218,11 @@ def evaluate_objects(audit_log):
                 print(object_path, "last accessed {} ago, greater than {} days(s) ago".format(
                     timedelta, config['DAYS_THRESHOLD']))
                 archive_futures.append(
-                    executor.submit(_archive_object, row, bucket_name,
-                                    object_name, object_path))
+                    executor.submit(archive_object, row.resourceName, bucket_name, object_name, object_path))
             else:
                 print(object_path, "last accessed {} ago, less than {} days(s) ago".format(
                     timedelta, config['DAYS_THRESHOLD']))
+
         # print results as they come
         for f in as_completed(archive_futures):
             print(f.result())
@@ -239,83 +232,12 @@ def evaluate_objects(audit_log):
         cleanup()
 
 
-def get_bucket_and_object(resource_name):
-    """Given an audit log resourceName, parse out the bucket name and object path within the bucket.
-
-    Returns:
-        (str, str) -- ([bucket name], [object name])
-    """
-    pathparts = resource_name.split("buckets/", 1)[1].split("/", 1)
-
-    bucket_name = pathparts[0]
-    object_name = pathparts[1].split("objects/", 1)[1]
-
-    return (bucket_name, object_name)
-
-
-clients = {}
-
-
-def get_bq_client():
-    """Get a BigQuery client. Uses a simple create-if-not-found mechanism to avoid repeatedly creating new clients.
-
-    Returns:
-        google.cloud.bigquery.Client -- A BigQuery client.
-    """
-    if 'bq' not in clients:
-        bq = bigquery.Client()
-        clients['bq'] = bq
-    return clients['bq']
-
-
-def get_gcs_client():
-    """Get a GCS client. Uses a simple create-if-not-found mechanism to avoid repeatedly creating new clients.
-
-    Returns:
-        google.cloud.storage.Client -- A GCS client.
-    """
-    if 'gcs' not in clients:
-        gcs = storage.Client()
-        clients['gcs'] = gcs
-    return clients['gcs']
-
-
-def event_is_fresh(data, context):
-    """Ensure a background Cloud Function only executes within a certain
-    time period after the triggering event.
-
-    Args:
-        data (dict): The event payload.
-        context (google.cloud.functions.Context): The event metadata.
-    Returns:
-        None; output is written to Stackdriver Logging
-    """
-    if data is None:
-        # desktop run
-        return True
-
-    timestamp = context.timestamp
-
-    event_time = parser.parse(timestamp)
-    event_age = (datetime.now(timezone.utc) - event_time).total_seconds()
-    event_age_ms = event_age * 1000
-
-    # Ignore events that are too old
-    # TODO: Should this be configurable?
-    max_age_ms = 10000
-    if event_age_ms > max_age_ms:
-        print('Event timeout. Dropping {} (age {}ms)'.format(
-            context.event_id, event_age_ms))
-        return False
-    return True
-
-
 def archive_cold_objects(data, context):
     if event_is_fresh(data, context):
-        print("Loading config.")
-        load_config()
         print("Initializing moved objects table (if not found).")
         initialize_moved_objects_table()
+        print("Initializing excluded objects table (if not found).")
+        initialize_excluded_objects_table()
         print("Getting access log, except for already moved objects.")
         audit_log = query_access_table()
         print("Evaluating accessed objects for rewriting to {}.".format(
