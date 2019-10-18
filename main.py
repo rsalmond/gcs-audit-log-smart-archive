52243ed520abd4b9fe261cd1d10f95b34f499f4b
Dom Zippilli <domz@google.com>
2019-10-18 18:53:57 +0000

    mergefix syntax mistake fixed

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ def archive_cold_objects(data, context):
         if hasattr(logging, config.get('LOG_LEVEL')):
             logging.getLogger("smart_archiver").setLevel(getattr(logging, config.get('LOG_LEVEL')))
         else:
-            print("Invalid log level specified: {}".format(config.get('LOG_LEVEL'))
+            print("Invalid log level specified: {}".format(config.get('LOG_LEVEL')))
             exit(1)
         LOG.debug("Configuration: \n %s", config)
         LOG.info("Evaluating accessed objects for rewriting to %s.",
